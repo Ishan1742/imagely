bb64ac2fa1595a08b9848306a9eb0dcfe1d42600
Ishan Ghosh <ghosh.ishang@gmail.com>
2020-11-04 12:10:49 +0530

    Add vue frontend

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+from flask_pymongo import PyMongo
 import lib.metadata as metadata
 from lib.encoder import CustomEncoder
 
 app = Flask(__name__)
+cors = CORS(app)
 app.json_encoder = CustomEncoder
+app.config["MONGO_URI"] = "mongodb://localhost:5200/imagedb"
+mongo = PyMongo(app)
 
 with open('./data/reel.jpg', mode='rb') as file:
     image = file.read()
@@ -19,11 +24,22 @@ images = [
     },
 ]
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('base.html')
 
 @app.route('/images')
 def hello():
     # print(metadata.extract_metadata('./data/reel.jpg'))
     return jsonify(images)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+    return render_template('upload-image.html')
 
-app.run(debug=True)
+@app.route('/images', methods=['POST'])
+def add_image():
+    image = request.get_json()
+
+
+app.run(debug=True, host='0.0.0.0')
---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="hello" />
+    <hr />
+    {{ hello }}
+    <hr />
+    <div v-for="(image,index) in images" :key=index>
+      {{ image }}
+      <hr />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      hello: "world",
+      images: {},
+    };
+  },
+  async mounted() {
+    await this.get();
+  },
+  methods: {
+    async get() {
+      let response = await fetch("http://localhost:5000/images");
+      let data = await response.json();
+      this.images = data;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
